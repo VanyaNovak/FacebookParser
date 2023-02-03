a4ac078ae5921507cd559dc516815334be501a0e
Ivan Novak <vananovak@ukr.net>
2023-02-03 14:37:00 +0200

    add test

--- spec/facebook_parser_spec.rb
+++ spec/facebook_parser_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 require "./facebook_parser"
+require 'faker'
 
 describe FacebookParser do
   describe "#contacts_list" do
@@ -9,6 +10,39 @@ describe FacebookParser do
       it "returns not authorized" do
         expect(subject).to eq "authorization failed"
       end
+    end
+
+    context "when authorized" do
+      let(:collected_names) do
+        10.times.map do
+          Faker::Name.name
+        end
+      end
+
+      let(:file_path) { "data.csv" }
+      let(:csv) do
+        CSV.open(file_path)
+      end
+
+      before do
+        allow_any_instance_of(FacebookParser).to receive(:auth).and_return(true)
+        allow_any_instance_of(FacebookParser).to receive(:collect_names).and_return(collected_names)
+      end
+
+      after do
+        File.delete(file_path)
+      end
+
+      it "generates csv file" do
+        subject
+
+        expect(csv.to_a.flatten).to match_array(
+          collected_names.each do |name|
+            name
+          end
+        )
+      end
+
     end
   end
 end
